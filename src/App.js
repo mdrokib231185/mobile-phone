4d4e32d900ab61b37c9af88c6a14378f211e2505
mdrokib231185 <mdrokhasan231185@gmail.com>
2022-03-25 23:39:50 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [mobile, setMobile] = useState([]);
+
+  useEffect(() => {
+            fetch('products.json')
+                  .then(res => res.json())
+                  .then(data => console.log(data))
+      }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+      return (
+            <div>
+                  <div className='header'>
+                        <h1 className='header1'>Mobile Phone Buy</h1> 
+                        <h2 className='header2'>Choice 4 clothes</h2>
+                </div>
+            </div>
+      );
+};
+
+export default Header;
